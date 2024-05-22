07920088406c920a44bb1a6b2f5d9a4108727166
drfailer <drfailer.42@gmail.com>
2024-05-22 11:39:33 -0400

    split matrix task

---
+++ src/task/split_matrix_task.h
@@ -0,0 +1,31 @@
+#ifndef SPLIT_MATRIX_TASK_H
+#define SPLIT_MATRIX_TASK_H
+#include "../data/matrix_block_data.h"
+#include "../data/matrix_data.h"
+#include <hedgehog/hedgehog.h>
+
+#define SMTaskInNb 1
+#define SMTaskIn MatrixData<T>
+#define SMTaskOut MatrixBlockData<T, Block>
+
+template <typename T>
+class SplitMatrixTask
+    : public hh::AbstractTask<SMTaskInNb, SMTaskIn, SMTaskOut> {
+public:
+  SplitMatrixTask()
+      : hh::AbstractTask<SMTaskInNb, SMTaskIn, SMTaskOut>("Split matrix task") {
+  }
+
+  void execute(std::shared_ptr<MatrixData<T>> matrix) override {
+    for (size_t iBlock = 0; iBlock < matrix->nbBlocksRows(); ++iBlock) {
+      for (size_t jBlock = 0; jBlock < matrix->nbBlocksCols(); ++jBlock) {
+        this->addResult(std::make_shared<MatrixBlockData<T, Block>>(
+            matrix->blockSize(), jBlock, iBlock, matrix->width(),
+            matrix->height(), &matrix->get()[iBlock * matrix->width() + jBlock],
+            matrix->get()));
+      }
+    }
+  }
+};
+
+#endif
