0a5cb78183b694057679662a18935feae1544e2c
drfailer <drfailer.42@gmail.com>
2024-05-22 11:25:14 -0400

    add full matrix dimentions in the matrix block + move constructor

--- src/data/matrix_block_data.h
+++ src/data/matrix_block_data.h
@@ -7,9 +7,16 @@
 
 template <typename T, BlockTypes BlockType> class MatrixBlockData {
 public:
-  MatrixBlockData(size_t blockSize, size_t x, size_t y, T *ptr, T *fullMatrix)
-      : blockSize_(blockSize), x_(x), y_(y), ptr_(ptr),
-        fullMatrix_(fullMatrix) {}
+  MatrixBlockData(size_t blockSize, size_t x, size_t y, size_t matrixWidth,
+                  size_t matrixHeight, T *ptr, T *fullMatrix)
+      : blockSize_(blockSize), x_(x), y_(y), matrixWidth_(matrixWidth),
+        matrixHeight_(matrixHeight), ptr_(ptr), fullMatrix_(fullMatrix) {}
+
+  template <typename Other, BlockTypes OtherType>
+  MatrixBlockData(std::shared_ptr<MatrixBlockData<Other, OtherType>> &&other)
+      : MatrixBlockData(other->blockSize(), other->x(), other->y(),
+                        other->matrixWidth(), other->matriHeight(),
+                        other->get(), other->fullMatrix()) {}
 
   size_t blockSize() const { return blockSize_; }
 
@@ -18,13 +25,22 @@ public:
   void x(size_t x) { this->x_ = x; }
   void y(size_t y) { this->y_ = y; }
 
+  size_t matrixWidth() const { return matrixWidth_; }
+  size_t matrixHeight() const { return matrixHeight_; }
+
   T *fullMatrix() { return fullMatrix_; }
   T *get() { return ptr_; }
+
+  // helper function (warn: we use i and j here and not x and y so it's
+  // inverted)
+  T &at(size_t i, size_t j) { return ptr_[i * matrixWidth_ + j]; }
 
 private:
   size_t blockSize_ = 0;
   size_t x_ = 0;
   size_t y_ = 0;
+  size_t matrixWidth_ = 0;
+  size_t matrixHeight_ = 0;
   T *ptr_ = nullptr;
   T *fullMatrix_ = nullptr;
 };
