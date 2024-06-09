f810ee6c9ffc944f442cfdb8866c0e974dfe69be
Chassagnol <rvc1@pn115038.campus.nist.gov>
2024-06-09 16:36:50 -0400

    update test script
    - now we can run the test for only one size

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,20 +1,26 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env bash                                                                                                                                                                                       [10/1923]
 ################################################################################
 #                               global variables                               #
 ################################################################################
 
-MATRIX_FILES_DIR=../../test-generator/scripts/cholesky/
-CHOLESKY=../build/cholesky-hh
-RESULT_OUTPUT_DIR=./results
+MATRIX_FILES_DIR=/scratch/rvc1/samples
+CHOLESKY=/scratch/rvc1/cholesky-hh
+RESULT_OUTPUT_DIR=/scratch/rvc1/results-hh
 
-BLOCK_SIZE_MIN=128
-BLOCK_SIZE_MAX=1024
+if [ $# -eq 1 ]; then
+  RESULT_OUTPUT_DIR=/scratch/rvc1/results-hh-$1
+fi
 
+BLOCK_SIZE_MIN=256
+BLOCK_SIZE_MAX=512
+NB_MEASURES=5
 # threads for the tasks.
 # strings: "d c u" (diagonal column update)
 declare -a THREADS=(
-  "1 8 32" "1 16 24" "1 20 20"
+  "1 8 384"
+  "1 16 384"
+  "1 32 384"
+  "1 64 384"
 )
 
 ################################################################################
@@ -23,6 +29,19 @@ declare -a THREADS=(
 
 mkdir -p $RESULT_OUTPUT_DIR
 lscpu > $RESULT_OUTPUT_DIR/cpuinfo.txt
+
+run_cholesky() {
+  inputfile=$1
+  graphname=$2                                                                                                                                                                                                       dThreads=$3
+  cThreads=$4
+  uThreads=$5
+  blockSize=$6
+  timefile=$7
+  echo "$CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize" >> $timefile
+  for ((i=0; i<$NB_MEASURES; i+=1)); do
+    $CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize >> $timefile
+  done
+}
 
 # test the program for different matrix sizes
 for threads in "${THREADS[@]}"; do
@@ -34,14 +53,14 @@ for threads in "${THREADS[@]}"; do
     dirPath=$RESULT_OUTPUT_DIR/$dThreads-$cThreads-$uThreads/$blockSize
     mkdir -p $dirPath
 
-    for file in $(ls $MATRIX_FILES_DIR); do
-      graphname=$dirPath/$(echo $file | sed 's/\.in$/.dot/')
-
-      echo "$CHOLESKY -i $MATRIX_FILES_DIR/$file -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize" >> $dirPath/times.txt
-      for i in {1..10}; do
-        $CHOLESKY -i $MATRIX_FILES_DIR/$file -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize >> $dirPath/times.txt
+    if [ $# -eq 1 ]; then
+      graphname=$dirPath/$1.dot
+      run_cholesky $MATRIX_FILES_DIR/$1.in $graphname $dThreads $cThreads $uThreads $blockSize $dirPath/times.txt
+    else
+      for file in $(ls $MATRIX_FILES_DIR); do
+        graphname=$dirPath/$(echo $file | sed 's/\.in$/.dot/')
+        run_cholesky $MATRIX_FILES_DIR/$file $graphname $dThreads $cThreads $uThreads $blockSize $dirPath/times.txt
       done
-
-    done
+    fi
   done
 done
