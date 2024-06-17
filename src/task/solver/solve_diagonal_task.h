1af9db2afdecfe7a9d26d0080a4320e4b140ff61
drfailer <drfailer.42@gmail.com>
2024-06-17 16:07:45 -0400

    Update the solver tasks with phases.

--- src/task/solver/solve_diagonal_task.h
+++ src/task/solver/solve_diagonal_task.h
@@ -1,6 +1,7 @@
 #ifndef SOLVE_DIAGONAL_TASK_H
 #define SOLVE_DIAGONAL_TASK_H
 #include "../../data/matrix_block_data.h"
+#include "../../data/solver/phases.h"
 #include <hedgehog/hedgehog.h>
 #include <cblas.h>
 
@@ -14,7 +15,7 @@ using SolveDiagonalTaskInType =
 #define SDTaskIn SolveDiagonalTaskInType<T>
 #define SDTaskOut MatrixBlockData<T, Vector>
 
-template <typename T>
+template <typename T, Phases Phase>
 class SolveDiagonalTask : public hh::AbstractTask<SDTaskInNb, SDTaskIn, SDTaskOut> {
  public:
   SolveDiagonalTask(size_t nbThreads)
@@ -24,15 +25,21 @@ class SolveDiagonalTask : public hh::AbstractTask<SDTaskInNb, SDTaskIn, SDTaskOu
     auto diagBlock = blocks->first;
     auto vecBlock = blocks->second;
     // todo: leading dimension should be configurable
-    cblas_dtrsm(CblasRowMajor, CblasRight, CblasLower, CblasNoTrans, CblasNonUnit,
-                vecBlock->height(), vecBlock->width(), 1.0, diagBlock->get(),
-                diagBlock->matrixWidth(), vecBlock->get(), vecBlock->matrixWidth());
+    if constexpr (Phase == Phases::First) {
+      cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit,
+                  vecBlock->height(), vecBlock->width(), 1.0, diagBlock->get(),
+                  diagBlock->matrixWidth(), vecBlock->get(), vecBlock->matrixWidth());
+    } else {
+      cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasTrans, CblasNonUnit,
+                  vecBlock->height(), vecBlock->width(), 1.0, diagBlock->get(),
+                  diagBlock->matrixWidth(), vecBlock->get(), vecBlock->matrixWidth());
+    }
     this->addResult(vecBlock);
   }
 
   std::shared_ptr<hh::AbstractTask<SDTaskInNb, SDTaskIn, SDTaskOut>>
   copy() override {
-    return std::make_shared<SolveDiagonalTask<T>>(this->numberThreads());
+    return std::make_shared<SolveDiagonalTask<T, Phase>>(this->numberThreads());
   }
 };
 
--- src/task/solver/update_vector_task.h
+++ src/task/solver/update_vector_task.h
@@ -2,6 +2,7 @@
 #define UPDATE_VECTOR_TASK_H
 #include "../../data/matrix_block_data.h"
 #include "../../data/triple_block_data.h"
+#include "../../data/solver/phases.h"
 #include <hedgehog/hedgehog.h>
 #include <cblas.h>
 
@@ -12,7 +13,9 @@ using UpdateVectorTaskInType = TripleBlockData<T, Column, Vector, Vector>;
 #define UVTaskIn UpdateVectorTaskInType<T>
 #define UVTaskOut MatrixBlockData<T, Updated>
 
-template <typename T>
+/// @brief Update the result vector by substracting the matrix times a solved vector block (removes
+/// known variables from the equation)
+template <typename T, Phases Phase>
 class UpdateVectorTask : public hh::AbstractTask<UVTaskInNb, UVTaskIn, UVTaskOut> {
  public:
   UpdateVectorTask(size_t nbThreads)
@@ -25,17 +28,23 @@ class UpdateVectorTask : public hh::AbstractTask<UVTaskInNb, UVTaskIn, UVTaskOut
     auto updatedBlock = blocks->third;       // vector block that we want to update
     size_t m = updatedBlock->height();
     size_t n = updatedBlock->width();
-    size_t k = colBlock->width();
+    size_t k = solvedVectorBlock->height();
     // todo: the leading dimension should be configurable
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, -1.0, colBlock->get(),
-                colBlock->matrixWidth(), solvedVectorBlock->get(), solvedVectorBlock->matrixWidth(), 1.0,
-                updatedBlock->get(), updatedBlock->matrixWidth());
+    if constexpr (Phase == Phases::First) {
+      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, -1.0, colBlock->get(),
+                  colBlock->matrixWidth(), solvedVectorBlock->get(), solvedVectorBlock->matrixWidth(), 1.0,
+                  updatedBlock->get(), updatedBlock->matrixWidth());
+    } else {
+      cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, m, n, k, -1.0, colBlock->get(),
+                  colBlock->matrixWidth(), solvedVectorBlock->get(), solvedVectorBlock->matrixWidth(), 1.0,
+                  updatedBlock->get(), updatedBlock->matrixWidth());
+    }
     this->addResult(std::make_shared<MatrixBlockData<T, Updated>>(updatedBlock));
   }
 
   std::shared_ptr<hh::AbstractTask<UVTaskInNb, UVTaskIn, UVTaskOut>>
   copy() override {
-    return std::make_shared<UpdateVectorTask<T>>(this->numberThreads());
+    return std::make_shared<UpdateVectorTask<T, Phase>>(this->numberThreads());
   }
 };
 
