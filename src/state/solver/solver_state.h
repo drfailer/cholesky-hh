83d62eada61edc43671abde731f2fdc16e228f31
drfailer <drfailer.42@gmail.com>
2024-06-17 16:04:19 -0400

    2 phases in the solver state.
    - Now the solver state as a new template parameter to model the phase.
    - phase1: we resolve the sub-problem using the lower triangular matrix.
    - phase2: we resolve the sub-problem with the upper triangular matrix to obtain the final solution.

--- src/state/solver/solver_state.h
+++ src/state/solver/solver_state.h
@@ -1,123 +1,190 @@
 #ifndef SOLVER_STATE_H
 #define SOLVER_STATE_H
+
 #include "../../data/matrix_block_data.h"
+#include "../../data/solver/phases.h"
 #include "../../task/solver/update_vector_task.h"
 #include "../../task/solver/solve_diagonal_task.h"
 #include <vector>
 #include <list>
 
-#define SStateInNb 4
-#define SStateIn \
-  MatrixBlockData<T, Result>, MatrixBlockData<T, Block>, \
-  MatrixBlockData<T, Vector>, MatrixBlockData<T, Updated>
-#define SStateOut \
-  SolveDiagonalTaskInType<T>, UpdateVectorTaskInType<T>, \
+#define SStateInNb 5
+#define SStateIn                         \
+  MatrixBlockData<T, Decomposed>,        \
+  MatrixBlockData<T, VectorBlock>,       \
+  MatrixBlockData<T, VectorBlockPhase1>, \
+  MatrixBlockData<T, Vector>,            \
+  MatrixBlockData<T, Updated>
+#define SStateOut                         \
+  SolveDiagonalTaskInType<T>,             \
+  UpdateVectorTaskInType<T>,              \
+  MatrixBlockData<T, VectorBlockPhase1>,  \
   MatrixBlockData<T, Result>
 
-template <typename T>
-class SolverState : public hh::AbstractState<SStateInNb, SStateIn, SStateOut> {
+template <typename T, Phases Phase>
+class SolverState : public hh::AbstractState<SStateInNb, SStateIn, SStateOut > {
  public:
-  SolverState() : hh::AbstractState<SStateInNb, SStateIn, SStateOut>() {}
+  SolverState() : hh::AbstractState<SStateInNb, SStateIn, SStateOut >() {}
 
-  /* Result *******************************************************************/
+  /* Decomposed ***************************************************************/
 
-  /// @brief Recieves the blocks from the cholesky decomposition graph
-  void execute(std::shared_ptr<MatrixBlockData<T, Result>> block) override {
+  /// @brief Receives the blocks from the cholesky decomposition graph
+  void execute(std::shared_ptr<MatrixBlockData<T, Decomposed>> block) override {
     if (blocks_.size() == 0) {
       nbBlocksCols_ = block->nbBlocksCols();
       nbBlocksRows_ = block->nbBlocksRows();
-      blocks_ = std::vector<std::shared_ptr<MatrixBlockData<T, Block>>>(
-          block->nbBlocksCols() * block->nbBlocksRows(), nullptr);
+      blocks_ = std::vector<std::shared_ptr<MatrixBlockData<T, MatrixBlock>>>(
+              block->nbBlocksCols() * block->nbBlocksRows(), nullptr);
     }
 
-    block->rank(0);
-    blocks_[block->idx()] = std::make_shared<MatrixBlockData<T, Block>>(std::move(block));
+    blocks_[block->idx()] = std::make_shared<MatrixBlockData<T, MatrixBlock>>(block);
     sendSolveDiagPendings();
     sendUpdatePendings();
   }
 
-  /* Block ********************************************************************/
+  /* VectorBlock **************************************************************/
 
-  /// @brief Recieves the vector blocks from the split matrix task
-  void execute(std::shared_ptr<MatrixBlockData<T, Block>> vecBlock) override {
-    if (vectorBlocks_.size() == 0) {
-      vectorBlocksTtl_ = vecBlock->nbBlocksRows();
-      vectorBlocks_ = std::vector<std::shared_ptr<MatrixBlockData<T, Vector>>>(
-          vecBlock->nbBlocksRows(), nullptr);
+  /// @brief Receives the vector blocks from the split matrix task
+  void execute(std::shared_ptr<MatrixBlockData<T, VectorBlock>> vecBlock) override {
+    if constexpr (Phase == Phases::First) {
+      if (vectorBlocks_.size() == 0) {
+        vectorBlocks_ = std::vector<std::shared_ptr<MatrixBlockData<T, Vector>>>(
+                vecBlock->nbBlocksRows(), nullptr);
+      }
+
+      vectorBlocks_[vecBlock->idx()] = std::make_shared<MatrixBlockData<T, Vector>>(vecBlock);
+
+      if (vecBlock->rank() == vecBlock->y()) {
+        solveDiagPendings_.emplace_back(SolveDiagonalIdx(
+                vecBlock->y() * nbBlocksCols_ + vecBlock->y(),
+                vecBlock->idx()
+        ));
+      }
+      sendSolveDiagPendings();
+      sendUpdatePendings();
     }
+  }
 
-    --vectorBlocksTtl_;
-    vectorBlocks_[vecBlock->idx()] = std::make_shared<MatrixBlockData<T, Vector>>(vecBlock);
+  /* VectorBlockPhase1 ********************************************************/
 
-    if (vecBlock->rank() == vecBlock->y()) {
-      solveDiagPendings_.emplace_back(SolveDiagonalIdx(
-            vecBlock->y() * nbBlocksCols_ + vecBlock->y(),
-            vecBlock->idx()
-          ));
+  /// @brief Receives the vector blocks from the phase 1
+  void execute(std::shared_ptr<MatrixBlockData<T, VectorBlockPhase1>> vecBlock) override {
+    if constexpr (Phase == Phases::Second) {
+      if (vectorBlocks_.size() == 0) {
+        vectorBlocks_ = std::vector<std::shared_ptr<MatrixBlockData<T, Vector>>>(
+                vecBlock->nbBlocksRows(), nullptr);
+      }
+
+      vecBlock->rank(nbBlocksRows_ - vecBlock->y());
+      vectorBlocks_[vecBlock->idx()] = std::make_shared<MatrixBlockData<T, Vector>>(vecBlock);
+
+      if (vecBlock->rank() == 1) {
+        solveDiagPendings_.emplace_back(SolveDiagonalIdx(
+                vecBlock->y() * nbBlocksCols_ + vecBlock->y(),
+                vecBlock->idx()
+        ));
+      }
+      sendSolveDiagPendings();
+      sendUpdatePendings();
     }
-    sendSolveDiagPendings();
-    sendUpdatePendings();
   }
 
   /* Vector *******************************************************************/
 
+  /// @brief Receives vector blocks from the solve diagonal task (these are results of the graph)
   void execute(std::shared_ptr<MatrixBlockData<T, Vector>> block) override {
-    blocks_[block->idx()]->incRank();
+    if constexpr (Phase == Phases::First) {
+     vectorBlocks_[block->idx()]->incRank();
 
-    for (size_t i = block->y() + 1; i < nbBlocksRows_; ++i) {
-      size_t colBlockIdx = i * nbBlocksCols_ + block->y() + 1;
-      updateVecPendings_.emplace_back(UpdateVectorIdx(
-            colBlockIdx,
-            block->idx(),
-            i
-          ));
+      for (size_t i = block->y() + 1; i < nbBlocksRows_; ++i) {
+        size_t colBlockIdx = i * nbBlocksCols_ + block->y();
+        updateVecPendings_.emplace_back(UpdateVectorIdx(
+                colBlockIdx,
+                block->idx(),
+                i
+        ));
+      }
+
+      this->addResult(std::make_shared<MatrixBlockData<T, VectorBlockPhase1>>(block));
+    } else {
+      vectorBlocks_[block->idx()]->decRank();
+
+      for (size_t i = 0; i < block->y(); ++i) {
+        size_t colBlockIdx = block->y() * nbBlocksCols_ + i; // we invert because the matrix should be translated
+        updateVecPendings_.emplace_back(UpdateVectorIdx(
+                colBlockIdx,
+                block->idx(),
+                i
+        ));
+      }
+
+     this->addResult(std::make_shared<MatrixBlockData<T, Result>>(block));
     }
-    this->addResult(std::make_shared<MatrixBlockData<T, Result>>(block));
     sendUpdatePendings();
   }
 
   /* Updated ******************************************************************/
 
+  /// @brief Receives vector blocs from the update vector task.
   void execute(std::shared_ptr<MatrixBlockData<T, Updated>> block) override {
-    blocks_[block->idx()]->incRank();
+    if constexpr (Phase == Phases::First) {
+      vectorBlocks_[block->idx()]->incRank();
+      block->incRank();
 
-    if (block->rank() == block->y()) {
-      solveDiagPendings_.emplace_back(SolveDiagonalIdx(
-            block->y() * nbBlocksCols_ + block->y(),
-            block->idx()
-          ));
+      if (block->rank() == block->y()) {
+        solveDiagPendings_.emplace_back(SolveDiagonalIdx(
+                block->y() * nbBlocksCols_ + block->y(),
+                block->idx()
+        ));
+      }
+    } else {
+      vectorBlocks_[block->idx()]->decRank();
+      block->decRank();
+
+      if (block->rank() == 1) {
+        solveDiagPendings_.emplace_back(SolveDiagonalIdx(
+                block->y() * nbBlocksCols_ + block->y(),
+                block->idx()
+        ));
+      }
     }
+
     sendSolveDiagPendings();
   }
 
   /* idDone ******************************************************************/
 
   [[nodiscard]] bool isDone() const {
-    return vectorBlocks_.size() && vectorBlocks_.back()->isProcessed();
+    if constexpr (Phase == Phases::First) {
+      return !vectorBlocks_.empty() && vectorBlocks_.back() &&
+             vectorBlocks_.back()->rank() > vectorBlocks_.back()->y();
+    } else {
+      return !vectorBlocks_.empty() && vectorBlocks_.front() && vectorBlocks_.front()->rank() == 0;
+    }
   }
 
  private:
   struct SolveDiagonalIdx {
-    SolveDiagonalIdx(size_t diag, size_t vec):
-      diag(diag), vec(vec) {}
+    SolveDiagonalIdx(size_t diag, size_t vec) :
+            diag(diag), vec(vec) {}
     size_t diag; // diagonal element
     size_t vec;  // vector element
   };
+
   struct UpdateVectorIdx {
-    UpdateVectorIdx(size_t col, size_t solvedVec, size_t updatedVec):
-      col(col), solvedVec(solvedVec), updatedVec(updatedVec) {}
+    UpdateVectorIdx(size_t col, size_t solvedVec, size_t updatedVec) :
+            col(col), solvedVec(solvedVec), updatedVec(updatedVec) {}
     size_t col;        // column element
     size_t solvedVec;  // solved vector part
     size_t updatedVec; // the part of the vector that we want to update
   };
-  std::vector<std::shared_ptr<MatrixBlockData<T, Block>>> blocks_ = {};
+
+  std::vector<std::shared_ptr<MatrixBlockData<T, MatrixBlock>>> blocks_ = {};
   std::vector<std::shared_ptr<MatrixBlockData<T, Vector>>> vectorBlocks_ = {};
   std::list<SolveDiagonalIdx> solveDiagPendings_ = {};
   std::list<UpdateVectorIdx> updateVecPendings_ = {};
   size_t nbBlocksCols_ = 0;
   size_t nbBlocksRows_ = 0;
-  size_t blocksTtl_ = 0;
-  size_t vectorBlocksTtl_ = 0;
 
   void sendUpdatePendings() {
     auto it = updateVecPendings_.begin();
@@ -127,11 +194,11 @@ class SolverState : public hh::AbstractState<SStateInNb, SStateIn, SStateOut> {
       auto solvedVec = vectorBlocks_[it->solvedVec];
       auto updatedVec = vectorBlocks_[it->updatedVec];
 
-      if (col) {
+      if (col && updatedVec) {
         this->addResult(std::make_shared<UpdateVectorTaskInType<T>>(
-              std::make_shared<MatrixBlockData<T, Column>>(col),
-              solvedVec,
-              updatedVec));
+                std::make_shared<MatrixBlockData<T, Column>>(col),
+                solvedVec,
+                updatedVec));
         it = updateVecPendings_.erase(it);
       } else {
         it++;
@@ -141,16 +208,20 @@ class SolverState : public hh::AbstractState<SStateInNb, SStateIn, SStateOut> {
 
 
   void sendSolveDiagPendings() {
+    if (blocks_.empty()) {
+      return;
+    }
+
     auto it = solveDiagPendings_.begin();
 
     while (it != solveDiagPendings_.end()) {
       auto diag = blocks_[it->diag];
-      auto vec = vectorBlocks_[it->diag];
+      auto vec = vectorBlocks_[it->vec];
 
-      if (diag) {
+      if (diag && vec) {
         this->addResult(std::make_shared<SolveDiagonalTaskInType<T>>(
-              std::make_shared<MatrixBlockData<T, Diagonal>>(diag),
-              vec));
+                std::make_shared<MatrixBlockData<T, Diagonal>>(diag),
+                vec));
         it = solveDiagPendings_.erase(it);
       } else {
         it++;
--- src/state/solver/solver_state_manager.h
+++ src/state/solver/solver_state_manager.h
@@ -3,17 +3,17 @@
 #include "solver_state.h"
 #include <hedgehog/hedgehog.h>
 
-template <typename T>
+template <typename T, Phases Phase>
 class SolverStateManager
     : public hh::StateManager<SStateInNb, SStateIn, SStateOut> {
 public:
-  explicit SolverStateManager(std::shared_ptr<SolverState<T>> const &state)
+  explicit SolverStateManager(std::shared_ptr<SolverState<T, Phase>> const &state)
       : hh::StateManager<SStateInNb, SStateIn, SStateOut>(
             state, "Solver State Manager") {}
 
     [[nodiscard]] bool canTerminate() const override {
         this->state()->lock();
-        auto ret = std::dynamic_pointer_cast<SolverState<T>>(this->state())->isDone();
+        auto ret = std::dynamic_pointer_cast<SolverState<T, Phase>>(this->state())->isDone();
         this->state()->unlock();
         return ret;
     }
