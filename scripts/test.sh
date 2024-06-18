d7eb54b757dbed50b9b2cbe1565edfb038244344
drfailer <drfailer.42@gmail.com>
2024-06-18 16:40:08 -0400

    Change the scripts so they can handle the new threads for the solver tasks.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,27 +1,30 @@
-#!/usr/bin/env bash                                                                                                                                                                                       [10/1923]
+#!/usr/bin/env bash
 
 ################################################################################
 #                               global variables                               #
 ################################################################################
 
-MATRIX_FILES_DIR=/scratch/rvc1/samples
-CHOLESKY=/scratch/rvc1/cholesky-hh
-RESULT_OUTPUT_DIR=/scratch/rvc1/results-hh
+MATRIX_FILES_DIR=../../test-generator/scripts/cholesky/
+CHOLESKY=../build/cholesky-hh
+RESULT_OUTPUT_DIR=./results-hh
 
 if [ $# -eq 1 ]; then
-  RESULT_OUTPUT_DIR=/scratch/rvc1/results-hh-$1
+  RESULT_OUTPUT_DIR=./results-hh-$1
 fi
 
 BLOCK_SIZE_MIN=256
-BLOCK_SIZE_MAX=512
-NB_MEASURES=5
+BLOCK_SIZE_MAX=256
+NB_MEASURES=10
 # threads for the tasks.
 # strings: "d c u" (diagonal column update)
 declare -a THREADS=(
-  "1 8 384"
-  "1 16 384"
-  "1 32 384"
-  "1 64 384"
+  "1 8 10 1 20"
+  "1 8 15 1 20"
+  "1 8 20 1 20"
+  "1 8 25 1 20"
+  "1 8 30 1 20"
+  "1 8 35 1 20"
+  "1 8 40 1 20"
 )
 
 ################################################################################
@@ -36,11 +39,14 @@ run_cholesky() {
   graphname=$2                                                                                                                                                                                                       dThreads=$3
   cThreads=$4
   uThreads=$5
-  blockSize=$6
-  timefile=$7
-  echo "$CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize" >> $timefile
+  sThreads=$6
+  vThreads=$7
+  blockSize=$8
+  timefile=$9
+  echo "$CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -s $sThreads -v $vThreads -b $blockSize"
+  echo "$CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -s $sThreads -v $vThreads -b $blockSize" >> $timefile
   for ((i=0; i<$NB_MEASURES; i+=1)); do
-    $CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -b $blockSize >> $timefile
+    $CHOLESKY -i $inputfile -g $graphname -d $dThreads -c $cThreads -u $uThreads -s $sThreads -v $vThreads -b $blockSize >> $timefile
   done
 }
 
@@ -49,18 +55,20 @@ for threads in "${THREADS[@]}"; do
   dThreads=$(echo $threads | awk -F' ' '{ print $1 }')
   cThreads=$(echo $threads | awk -F' ' '{ print $2 }')
   uThreads=$(echo $threads | awk -F' ' '{ print $3 }')
+  sThreads=$(echo $threads | awk -F' ' '{ print $4 }')
+  vThreads=$(echo $threads | awk -F' ' '{ print $5 }')
 
   for ((blockSize=$BLOCK_SIZE_MIN; blockSize<=$BLOCK_SIZE_MAX; blockSize*=2)); do
-    dirPath=$RESULT_OUTPUT_DIR/$dThreads-$cThreads-$uThreads/$blockSize
+    dirPath=$RESULT_OUTPUT_DIR/$dThreads-$cThreads-$uThreads-$sThreads-$vThreads/$blockSize
     mkdir -p $dirPath
 
     if [ $# -eq 1 ]; then
       graphname=$dirPath/$1.dot
-      run_cholesky $MATRIX_FILES_DIR/$1.in $graphname $dThreads $cThreads $uThreads $blockSize $dirPath/times.txt
+      run_cholesky $MATRIX_FILES_DIR/$1.in $graphname $dThreads $cThreads $uThreads $sThreads $vThreads $blockSize $dirPath/times.txt
     else
       for file in $(ls $MATRIX_FILES_DIR); do
         graphname=$dirPath/$(echo $file | sed 's/\.in$/.dot/')
-        run_cholesky $MATRIX_FILES_DIR/$file $graphname $dThreads $cThreads $uThreads $blockSize $dirPath/times.txt
+        run_cholesky $MATRIX_FILES_DIR/$file $graphname $dThreads $cThreads $uThreads $sThreads $vThreads $blockSize $dirPath/times.txt
       done
     fi
   done
